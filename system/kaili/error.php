a8dec2fce3d8dbeb1fa7dcf7942ddf4a109ff0c4
Luigi Marco Simonetti <luigimarco@gmail.com>
2011-10-15 15:37:58 +0200

    added Error class

---
+++ system/kaili/error.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kaili;
+
+/**
+ * Kaili Config Class
+ *
+ * Class to manage config files
+ *
+ * @package Kaili
+ */
+class Error
+{
+    public static function show($message, $status_code = 500){
+        
+    }
+
+}
+
